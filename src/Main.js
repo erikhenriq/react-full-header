5edbb5bd953b161040026ce7d7191c3de201eb32
Erik <erik.hac@outlook.com>
2019-09-29 22:36:29 -0300

    Add first test

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Component = ({ name }) => (
-    <h1>Hello {name}!</h1>
+const FullHeader = () => (
+    <header></header>
 );
 
-Component.propTypes = {
+FullHeader.propTypes = {
     name: PropTypes.string.isRequired,
 };
 
-export default Component;
+export default FullHeader;

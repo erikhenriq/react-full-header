edaec2d8057b71432938114507ca9cefcc248cf4
Erik <erik.hac@outlook.com>
2019-09-30 07:47:58 -0300

    Add styles, bgImg and video

--- stories/Main.js
+++ stories/Main.js
@@ -43,6 +43,6 @@ storiesOf('React Full Header', module)
             subtitle="Curso de JS com TDD na prática."
             bgColor="#EBE9EB"
             textColor="#3299BB"
-            video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
+            video="https://storage.googleapis.com/coverr-main/mp4%2FCaramoan%20Drone.mp4?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=coverr-183014%40appspot.gserviceaccount.com%2F20190930%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20190930T104534Z&X-Goog-Expires=300&X-Goog-SignedHeaders=host&X-Goog-Signature=5ed2ac225bdbd3cd0db8eec09129ab42f8d0bb6c10bbecc9c8dd9deb0c544c6b696f6a664c5062f8ac09d353b4b878dcea42c6f89ce1d0363e994ae7aca01ed5098ddbbfa54ab60eadc2971e9023b4145eee1d1b4f87c81b6e70aa678ec5d506ead1615c4fc664495ae9439dae0d0b214f713d07a7320b680db41fd45fb44839031be000dea412cc126d537f7c77ed10e57f3f75672dfae2127d04a462afd8c1c7aa1593d3cfa55c95919a4143517d975ed137abbc3426a104913458ebd392d89ac1c4e296d9aa5adbbfba3235082d937ee9ead792769d3d6e93f7295c87f32c1c9c4afe3d39fab17a5b2fcafe8bf638b550ba51c1a1822faaf3125dcf3a2ef9"
         />
     ));
--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -54,14 +54,67 @@ describe('<FullHeader />', () => {
     });
 
     context('bgColor', () => {
-        it('should have background-color #ccc then none passed', () => {
+        it('should have background-color #ccc when none passed', () => {
             const wrapper = shallow(<FullHeader title="TDD" />);
             expect(wrapper).to.have.style('background-color').equal('#ccc');
         });
 
-        it('should have background-color equal #000 when none is passed', () => {
+        it('should have background-color equal to #000 when passed', () => {
             const wrapper = shallow(<FullHeader title="TDD" bgColor="#000" />);
             expect(wrapper).to.have.style('background-color').equal('#000');
+        });
+    });
+
+    context('textColor', () => {
+        it('should have color #fff when none passed', () => {
+            const wrapper = shallow(<FullHeader title="TDD" />);
+            expect(wrapper).to.have.style('color').equal('#fff');
+        });
+
+        it('should have text-color equal to #000 when passed', () => {
+            const wrapper = shallow(<FullHeader title="TDD" textColor="#000" />);
+            expect(wrapper).to.have.style('color').equal('#000');
+        });
+    });
+
+    context('font', () => {
+        it('should have font equal sans-serif when none passed', () => {
+            const wrapper = shallow(<FullHeader title="TDD" />);
+            expect(wrapper).to.have.style('font-family').equal('sans-serif');
+        });
+
+        it('should have font equal open-sans when passed', () => {
+            const wrapper = shallow(<FullHeader title="TDD" font="open-sans" />);
+            expect(wrapper).to.have.style('font-family').equal('open-sans');
+        });
+    });
+
+    context('bgImg', () => {
+        it('should have background-image equal to empty when none passed', () => {
+            const wrapper = shallow(<FullHeader />);
+            expect(wrapper).to.have.style('background-image').equal('url()');
+        });
+
+        it('should have background-image equal to bg.jpg when passed', () => {
+            const wrapper = shallow(<FullHeader bgImg="bg.jpg" />);
+            expect(wrapper).to.have.style('background-image').equal('url(bg.jpg)');
+        });
+    });
+
+    context('video', () => {
+        it('should have video tag when video is passed', () => {
+            const wrapper = shallow(<FullHeader video="my_video.mp4" />);
+            expect(wrapper.find('video')).to.have.length(1);
+        });
+
+        it('should not have video tag when video is not passed', () => {
+            const wrapper = shallow(<FullHeader />);
+            expect(wrapper.find('video')).to.have.length(0);
+        });
+
+        it('should have video tag with the video passed', () => {
+            const wrapper = shallow(<FullHeader video="my_video.mp4" />);
+            expect(wrapper.find('video').props().src).to.be.equal('my_video.mp4');
         });
     });
 });
